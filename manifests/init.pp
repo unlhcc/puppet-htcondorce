00d7d366b180e15e2a7a0c5406175df7394bee1a
Derek Weitzel <djw8605@gmail.com>
2015-08-28 10:13:48 -0500

    Validating the backend scheduler

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,8 @@ class htcondorce (
   $squid = $slurm::params::squid,
   $backend_scheduler = $slurm::params::backend_scheduler
   ) inherits slurm::params {
+
+    validate_re($backend_scheduler, [ '^pbs$', '^condor$', '^lsf$', '^sge$' ], "Error, backend_scheduler must be either pbs, condor, lsf, or sge")
     
     package { "htcondor-ce-${::backend_scheduler}": ensure => present }
 
