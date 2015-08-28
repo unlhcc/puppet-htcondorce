b6ba57106d52cb278aba22284887775b93a251f3
Derek Weitzel <djw8605@gmail.com>
2015-08-28 14:26:46 -0500

    Adding slurm support

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,19 @@ class htcondorce (
   $backend_scheduler = $htcondorce::params::backend_scheduler,
   ) inherits htcondorce::params {
     
+    if $backend_scheduler =~ /^slurm$/ {
+        package { "gratia-probe-slurm": ensure => present }
+        package { "osg-configure-slurm" : ensure => present }
+        $backend_scheduler = "pbs"
+    }
+
     validate_re($backend_scheduler, [ '^pbs$', '^condor$', '^lsf$', '^sge$' ], "Error, backend_scheduler must be either pbs, condor, lsf, or sge")
     
-    package { "htcondor-ce-${backend_scheduler}": ensure => present }
+    package { "osg-ce-${backend_scheduler}": 
+      ensure => present
+    }
+
+    
+    
 
 }
