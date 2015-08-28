b427a9a5527107e8b8a2cc4cfc186ebed37d98c0
Derek Weitzel <djw8605@gmail.com>
2015-08-28 10:44:11 -0500

    Removing references to slurm, oops

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,9 @@
 
 class htcondorce (
-  $squid = $slurm::params::squid,
-  $backend_scheduler = $slurm::params::backend_scheduler
-  ) inherits slurm::params {
-    notice( "Backend scheduler set to ${backend_scheduler}" )
+  $squid = $htcondorce::params::squid,
+  $backend_scheduler = $htcondorce::params::backend_scheduler,
+  ) inherits htcondorce::params {
+    
     validate_re($backend_scheduler, [ '^pbs$', '^condor$', '^lsf$', '^sge$' ], "Error, backend_scheduler must be either pbs, condor, lsf, or sge")
     
     package { "htcondor-ce-${backend_scheduler}": ensure => present }
