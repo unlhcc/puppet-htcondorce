85a00e9c1651d6cfd2615671eabbbee112f37b5f
Derek Weitzel <djw8605@gmail.com>
2015-08-28 10:36:33 -0500

    Adding debugging to the backend_scheduler

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,9 @@ class htcondorce (
   $squid = $slurm::params::squid,
   $backend_scheduler = $slurm::params::backend_scheduler
   ) inherits slurm::params {
-
+    notice( "Backend scheduler set to ${backend_scheduler}" )
     validate_re($backend_scheduler, [ '^pbs$', '^condor$', '^lsf$', '^sge$' ], "Error, backend_scheduler must be either pbs, condor, lsf, or sge")
     
-    package { "htcondor-ce-${::backend_scheduler}": ensure => present }
+    package { "htcondor-ce-${backend_scheduler}": ensure => present }
 
 }
