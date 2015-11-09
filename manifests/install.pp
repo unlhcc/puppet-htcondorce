f09a9b7fc843cad3adf02ca387b5283c53edccdc
hzhu16 <nautilushj@gmail.com>
2015-11-09 13:19:26 -0600

    install.pp update

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,7 +8,7 @@ class htcondorce::install (
       path        => "/bin:/usr/bin:/sbin:/usr/sbin",
       command     => "yum update",
       subscribe   => File["/etc/pki/rpm-gpg/RPM-GPG-KEY-OSG"],
-      refreshonly => "TRUE",
+      refreshonly => true,
       logoutput   => "on_failure",
     }
 }
